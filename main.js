7d079651682d5757c2060dbfc95d26105309c17d
Franky <56810823+Franky-Lim24@users.noreply.github.com>
2020-12-31 00:44:37 +0800

    Angular

--- main.js
+++ main.js
@@ -1,20 +1,40 @@
-const electron = require('electron');
-const url = require('url');
-const path = require('path');
+const electron = require("electron");
+const url = require("url");
+const path = require("path");
 
 const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
-app.on('ready', function () {
-	//Create new window
-	mainWindow = new BrowserWindow({});
-	//Load html into window
-	mainWindow.loadURL(
-		url.format({
-			pathname: path.join(__dirname, 'mainWindow.html'),
-			protocol: 'file:',
-			slashes: true,
-		})
-	);
-	Menu.setApplicationMenu(null);
+function createWindow() {
+  //Create new window
+  mainWindow = new BrowserWindow({
+    backgroundColor: "#ffffff",
+    icon: "assets/logo.png",
+  });
+  //Load html into window
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "/dist/index.html"),
+      protocol: "file:",
+      slashes: true,
+    })
+  );
+  mainWindow.on("closed", function () {
+    mainWindow = null;
+  });
+  mainWindow.webContents.openDevTools();
+  Menu.setApplicationMenu(null);
+}
+app.on("ready", createWindow);
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", function () {
+  if (mainWindow == null) {
+    createWindow();
+  }
 });
